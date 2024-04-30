fd5d50f531a4314f52678997b7cb2d29ee60f7a2
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2024-05-01 03:26:03 +0700

    [UPDATE] Create Data & Form Validation

--- components/button.tsx
+++ components/button.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import { IoAddSharp, IoPencil, IoTrashOutline } from "react-icons/io5";
+import { useFormStatus } from "react-dom";
+import clsx from "clsx";
 
 export const CreateButton = () => {
   return (
     <Link
-      href="/contact/create"
+      href="/contacts/create"
       className="inline-flex items-center space-x-1 text-white bg-blue-700 hover:bg-blue-800 px-5 py-[9px] rounded-sm text-sm"
     >
       <IoAddSharp size={20} />
@@ -16,7 +20,7 @@ export const CreateButton = () => {
 export const EditButton = () => {
   return (
     <Link
-      href="/contact/create"
+      href="/contacts/create"
       className="rounded-sm border p-1 hover:bg-gray-100"
     >
       <IoPencil size={20} />
@@ -28,6 +32,27 @@ export const DeleteButton = () => {
   return (
     <button className="rounded-sm border p-1 hover:bg-gray-100">
       <IoTrashOutline size={20} />
+    </button>
+  );
+};
+
+export const SubmitButton = ({ label }: { label: string }) => {
+  const { pending } = useFormStatus();
+
+  const className = clsx(
+    "text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-sm text-sm w-full px-5 py-3 text-center",
+    {
+      "opacity-50 cursor-progress": pending,
+    }
+  );
+
+  return (
+    <button type="submit" className={className} disabled={pending}>
+      {label === "save" ? (
+        <span>{pending ? "Saving..." : "Save"}</span>
+      ) : (
+        <span>{pending ? "Updating..." : "Update"}</span>
+      )}
     </button>
   );
 };
---
+++ lib/action.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { z } from "zod";
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const ContactSchema = z.object({
+  name: z.string().min(3),
+  phone: z.string().min(10),
+});
+
+export const saveContact = async (prevState: any, formData: FormData) => {
+  const validatedField = ContactSchema.safeParse(
+    Object.fromEntries(formData.entries())
+  );
+  if (!validatedField.success) {
+    return {
+      Error: validatedField.error.flatten().fieldErrors,
+    };
+  }
+  try {
+    await prisma.contact.create({
+      data: {
+        name: validatedField.data.name,
+        phone: validatedField.data.phone,
+      },
+    });
+  } catch (error) {
+    return { message: "Failed to create contact" };
+  }
+  revalidatePath("/contacts");
+  redirect("/contacts");
+};
